dafcfbe9a2d15b8f285dc4dadbc9a0c036194259
dryjorse <kozoevabdulazim@gmail.com>
2023-01-03 20:18:25 +0300

    back

---
+++ models/genres.js
@@ -0,0 +1,105 @@
+const genres = {
+    count: 10,
+    result: [
+        {
+            name: "комедия",
+            url: "/genres/comedy",
+        },
+        {
+            name: "фэнтези",
+            url: "/genres/fantasy"
+        },
+        {
+            name: 'демоны',
+            url: "/genres/demons"
+        },
+        {
+            name: "школа",
+            url: "/genres/school"
+        },
+        {
+            name: "ужасы",
+            url: "/genres/horror"
+        },
+        {        
+            name: "боевик",
+            url: "/genres/action"
+        },
+        {
+            name: "спорт",
+            url: "/genres/sport"
+        },
+        {
+            name: "драма",
+            url: "/genres/drama"
+        },
+        {
+            name: "романтика",
+            url: "/genres/romance"
+        },
+        {
+            name: "приключение",
+            url: "/genres/romance"
+        }
+    ]
+}
+    
+
+
+const allGenres = [
+    {
+        id: 1,
+        name: "комедия",
+        url: "/genres/comedy",
+    },
+    {
+        id: 2,
+        name: "фэнтези",
+        url: "/genres/fantasy"
+    },
+    {
+        id: 3,
+        name: 'демоны',
+        url: "/genres/demons"
+    },
+    {
+        id: 4,
+        name: "школа",
+        url: "/genres/school"
+    },
+    {
+        id: 5,
+        name: "ужасы",
+        url: "/genres/horror"
+    },
+    {        
+        id: 6,
+        name: "боевик",
+        url: "/genres/action"
+    },
+    {
+        id: 7,
+        name: "спорт",
+        url: "/genres/sport"
+    },
+    {
+        id: 8,
+        name: "драма",
+        url: "/genres/drama"
+    },
+    {
+        id: 9,
+        name: "романтика",
+        url: "/genres/romance"
+    },
+    {
+        id: 10,
+        name: "приключение",
+        url: "/genres/romance"
+    }
+]
+
+module.exports = {
+    genres,
+    allGenres
+}
