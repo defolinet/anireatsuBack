09fc3bcd4c264d34f4c97a5f0d562ce1d2c0d57f
dryjorse <kozoevabdulazim@gmail.com>
2023-01-10 23:02:46 +0300

    change

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.get('/anime', (req, res) => {
     let animesSend = {
         ...animes, 
         count: searched.length,
-        next: `/anime?offset=${offset + limit}&limit=${limit}`,
+        oldest: allAnime.reduce((prev, e) => {
+            if(e.release.year < prev.release.year) return e
+            else return prev
+        }),
+        newest: allAnime.reduce((prev, e) => {
+            if(e.release.year > prev.release.year) return e
+            else return prev
+        }),
         result: searched.filter((e, key) => key >= offset && key < (offset + limit))
     }
     if(offset >= limit){
@@ -29,8 +36,6 @@ app.get('/anime', (req, res) => {
     }
     res.send(animesSend)
 })
-
-console.log(allAnime.filter(e => e.genres.some(el => el.name === 'приключение')).length);
 
 app.get('/anime/:selector', (req, res) => {
     let selector = req.params.selector
